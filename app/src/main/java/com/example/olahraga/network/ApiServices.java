967765a9a163f9236bbc6ad9c606b736c273edb7
Kedok <zaenudinm844@gmail.com>
2019-06-24 09:12:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/olahraga/network/ApiServices.java
@@ -0,0 +1,16 @@
+package com.example.olahraga.network;
+
+import com.example.olahraga.response.ResponseBerita;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiServices {
+
+    //@TIPEMETHOD("API_END_POINT")
+    @GET("tampil_berita.php")
+    Call<ResponseBerita> request_show_all_berita();
+    // <ModelData> nama_method()
+
+
+}
